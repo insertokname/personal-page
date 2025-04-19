ae025acff15e7e81c614c9b833b2f4380ddcb7bd
insertokname <insertokname@gmail.com>
2025-04-19 15:56:21 +0300

    made files select-none

--- components/FileList/FileListHeader.tsx
+++ components/FileList/FileListHeader.tsx
@@ -3,9 +3,9 @@ export default function FileListHeader() {
   return (
     <thead className="border-b border-gruvbox-bg2">
       <tr>
-        <th className="px-4 py-2 text-left text-xs uppercase tracking-wider w-1/2 text-gruvbox-blue">Name</th>
-        <th className="hidden md:table-cell px-4 py-2 text-left text-xs uppercase tracking-wider text-gruvbox-blue">Date modified</th>
-        <th className="hidden sm:table-cell px-4 py-2 text-right text-xs uppercase tracking-wider text-gruvbox-blue">Type</th>
+        <th className="select-none px-4 py-2 text-left text-xs uppercase tracking-wider w-1/2 text-gruvbox-blue">Name</th>
+        <th className="select-none hidden md:table-cell px-4 py-2 text-left text-xs uppercase tracking-wider text-gruvbox-blue">Date modified</th>
+        <th className="select-none hidden sm:table-cell px-4 py-2 text-right text-xs uppercase tracking-wider text-gruvbox-blue">Type</th>
       </tr>
     </thead>
   );
