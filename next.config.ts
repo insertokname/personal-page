432db8dc0545ebe20981c4eca5066f943bfee3c4
insertokername <iliedemian07@gmail.com>
2025-04-21 14:54:02 +0300

    hopefully fixed base path errors

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
+const isAction = process.env.DEPLOY_IS_GH_ACTION ==  'true';
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: 'export',
+  basePath: isAction? '/personal-page':'',
 };
 
 export default nextConfig;
