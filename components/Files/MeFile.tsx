39096789d2467dc088332c645a98675835ad6548
insertokname <insertokname@gmail.com>
2025-04-23 00:32:35 +0300

    updated me file

--- components/Files/MeFile.tsx
+++ components/Files/MeFile.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import FileIcon from '../icons/FileIcon';
 import { FileItem } from './types';
+import NixosIcon from '../icons/NixosIcon';
 
 export class MeFileContent extends React.Component {
     render() {
         return <div>
-            <h1>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</h1>
+            <h1>Hello!</h1>
+            <br />
+            <p>I am Ilie, a developer based in romania!</p>
+            <p>I have experience with c/c++ and rust, I also dabble in c# dart python JS/TS </p>
+            <br />
+            <p>Proud <NixosIcon className='inline align-middle' /> NixOS enjoyer </p>
         </div>
     }
 }
